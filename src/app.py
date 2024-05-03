1e211fd16fa87ea41be6f7f913bbe57dc683b5d3
pc <e0xextazy@gmail.com>
2024-05-03 18:21:52 +0300

    add secrets

--- src/app.py
+++ src/app.py
@@ -5,62 +5,16 @@ from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-from langchain.docstore.document import Document
-from langchain.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain, create_history_aware_retriever
-
-from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 
-from reader import get_data
-from retriever import create_retriever
-from toxicity_classifier import text2toxicity
+from nlu.toxicity_classifier import text2toxicity
+from rag import load_rag
 
-os.environ["OPENAI_API_KEY"] = ""
 
-txt_docs = get_data('data_txt')
-docs = [Document(page_content=txt_doc) for txt_doc in txt_docs]
-retriever = create_retriever(docs)
-llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125")
-
-contextualize_q_system_prompt = """Учитывая историю чатов и последний вопрос пользователя \
-который может ссылаться на контекст в истории чата, сформулируйте отдельный вопрос \
-который можно понять без истории чата. НЕ отвечайте на вопрос, \
-только переформулируйте его, если это необходимо, а в остальном верните его как есть."""
-contextualize_q_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", contextualize_q_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-history_aware_retriever = create_history_aware_retriever(
-    llm, retriever, contextualize_q_prompt
-)
-
-qa_system_prompt = """Ты умный ассистент, которого зовут Хьюстон. Ты любишь отвечать на вопросы пользователей. \
-Используйте следующие фрагменты найденного контекста для ответа на вопрос. \
-Если вы не знаете ответа, просто скажите, что не знаете. \
-Используйте не более трех предложений и будьте лаконичны в ответе. \
-
-{context}"""
-
-qa_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", qa_system_prompt),
-        MessagesPlaceholder("chat_history"),
-        ("human", "{input}"),
-    ]
-)
-question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
-rag_chain = create_retrieval_chain(
-    history_aware_retriever, question_answer_chain)
-
+rag_chain = load_rag()
 chat_history = collections.defaultdict(list)
 
-TOKEN = '6760822338:AAE1eeQA_RAPeglB09QqcuY57hfdC8dhxIQ'
-bot = Bot(token=TOKEN)
+bot = Bot(token=os.getenv("TGBOT_TOKEN"))
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
@@ -81,10 +35,10 @@ async def clear_memory(message: types.Message):
 async def handle_message(message: types.Message):
     message_text = message.text
     user_id = message.from_user.id
-    print(chat_history)
-
     toxicity = text2toxicity(message_text)
-    print("toxicity", toxicity, message_text)
+
+    # write to db context
+
     if toxicity > 0.5:
         await message.reply("Пожалуйста, будьте вежливы!")
     else:
---
+++ src/rag.py
@@ -0,0 +1,50 @@
+from langchain.docstore.document import Document
+from langchain_openai import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain, create_history_aware_retriever
+
+from reader import get_data
+from retriever import create_retriever
+
+
+def load_rag():
+    txt_docs = get_data('data_txt')
+    docs = [Document(page_content=txt_doc) for txt_doc in txt_docs]
+    retriever = create_retriever(docs)
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo-0125")
+
+    contextualize_q_system_prompt = """Учитывая историю чатов и последний вопрос пользователя \
+    который может ссылаться на контекст в истории чата, сформулируйте отдельный вопрос \
+    который можно понять без истории чата. НЕ отвечайте на вопрос, \
+    только переформулируйте его, если это необходимо, а в остальном верните его как есть."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, retriever, contextualize_q_prompt
+    )
+
+    qa_system_prompt = """Ты умный ассистент, которого зовут Хьюстон. Ты любишь отвечать на вопросы пользователей. \
+    Используйте следующие фрагменты найденного контекста для ответа на вопрос. \
+    Если вы не знаете ответа, просто скажите, что не знаете. \
+    Используйте не более трех предложений и будьте лаконичны в ответе. \
+
+    {context}"""
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+    rag_chain = create_retrieval_chain(
+        history_aware_retriever, question_answer_chain)
+
+    return rag_chain
