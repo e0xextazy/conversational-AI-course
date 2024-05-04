1b7c4258eefee0c2381cefff965c193b91362507
pc <e0xextazy@gmail.com>
2024-05-04 18:17:01 +0300

    update

--- src/test.py
+++ src/test.py
@@ -14,6 +14,7 @@ class Message:
 class BotSimulator:
     def __init__(self, pipeline):
         self.chat_history = collections.defaultdict(list)
+        self.chat_history_stats = collections.defaultdict(list)
         self.current_node = 'start_node'
         self.pipeline = pipeline
         self.nodes = {
@@ -25,6 +26,7 @@ class BotSimulator:
         }
 
     def transition(self, message):
+        flag, toxicity = self.is_toxic(message)
         if message.text == "/start":
             self.current_node = 'start_node'
         elif message.text == "/clear_memory":
@@ -33,14 +35,18 @@ class BotSimulator:
             self.current_node = 'stat_node'
         elif message.text == "/help":
             self.current_node = 'help_node'
-        elif self.current_node == 'chat_node' and self.is_toxic(message.text):
+        elif self.current_node == 'chat_node' and flag:
             self.current_node = 'toxic_node'
+            self.chat_history_stats[message.id].append(
+                (message.text, toxicity))
         else:
             self.current_node = 'chat_node'
+            self.chat_history_stats[message.id].append(
+                (message.text, toxicity))
 
-    def is_toxic(self, text):
-        toxicity = text2toxicity(text)
-        return toxicity > 0.5
+    def is_toxic(self, message):
+        toxicity = text2toxicity(message.text)
+        return toxicity > 0.5, toxicity
 
     def process_message(self, message):
         self.transition(message)
@@ -48,10 +54,19 @@ class BotSimulator:
 
     def clear_node(self, message):
         self.chat_history[message.id] = []
+        self.chat_history_stats[message.id] = []
         return "История сообщений удалена!"
 
     def stat_node(self, message):
-        return "ЧТО-ТО ПРО СТАТИСТИКУ"
+        stat = {}
+        history = self.chat_history_stats[message.id]
+        stat["len_history"] = len(history)
+        stat["avg_len_msg"] = int(
+            (sum([len(el[0].split()) for el in history]) / stat["len_history"]) * 100) / 100
+        stat["avg_toxic"] = int(
+            (sum([el[1] for el in history]) / stat["len_history"]) * 100) / 100
+
+        return repr(stat)
 
     def start_node(self, message):
         return "Привет! Я помощник Хьюстон. Задай мне вопрос!"
@@ -68,7 +83,9 @@ class BotSimulator:
         return "Пожалуйста, будьте вежливы!"
 
     def chat_node(self, message):
-        if self.is_toxic(message.text):
+
+        flag, _ = self.is_toxic(message)
+        if flag:
             return self.toxic_node(message)
 
         response = self.pipeline.invoke(
@@ -107,7 +124,8 @@ if __name__ == "__main__":
         (Message(text="/start", id=2), "Привет! Я помощник Хьюстон. Задай мне вопрос!"),
         (Message(text="Как дела?", id=2),
          "Всё отлично, спасибо! Как я могу помочь вам сегодня?"),
-        (Message(text="/get_stat", id=2), "ЧТО-ТО ПРО СТАТИСТИКУ"),
+        (Message(text="/get_stat", id=2),
+         "{'len_history': 1, 'avg_len_msg': 2.0, 'avg_toxic': 0.01}"),
         (Message(text="Что нового?", id=3),
          "С 2022 года Smart Consulting изменил отношение к развитию сотрудников, обещая помочь каждому на пути становления. Также в компании поощряют постоянное обучение и развитие."),
         (Message(text="пока", id=2),
